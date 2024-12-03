cbeaf8b0cb3161ff1a26afd8bc23e9137a0f9462
Vasudha Ghule <133812225+vasudhaghule@users.noreply.github.com>
2024-12-03 11:01:42 +0530

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,14 @@
 // Button.tsx
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  bgColor?: string; // Background color
-  textColor?: string; // Text color
+  bgColor?: string; 
+  textColor?: string; 
 }
 
 export const Button: React.FC<ButtonProps> = ({
-  bgColor = "#FFFFFF", // Default background color
-  textColor = "#000000", // Default text color
-  children, // Button text/content
-  ...props // Other button props
+  bgColor = "#FFFFFF", 
+  textColor = "#000000", 
+  children, 
+  ...props 
 }) => {
   return (
     <button
@@ -17,7 +17,7 @@ export const Button: React.FC<ButtonProps> = ({
         color: textColor,
       }}
       className=" py-3 px-5  border-2 border-black rounded-full font-medium text-sm transition-all hover:opacity-95"
-      {...props} // Spread additional button attributes
+      {...props} 
     >
       <span>{children}</span>
     </button>
