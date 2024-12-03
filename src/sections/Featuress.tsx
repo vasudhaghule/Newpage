95e81b71088aae35e98e3bc57794b1b11e306b64
Vasudha Ghule <133812225+vasudhaghule@users.noreply.github.com>
2024-12-03 10:59:25 +0530

    Update Featuress.tsx

--- src/sections/Featuress.tsx
+++ src/sections/Featuress.tsx
@@ -10,11 +10,8 @@ import FeatD from "@/assets/Featd.png";
 const Featuress = () => {
   return (
     <section className="mt-20 relative">
-      {/* Wrapper to control layout */}
       <div className="relative flex justify-start max-w-[1300px] h-[640px] mx-auto ">
-        {/* Content Container */}
         <div className="relative min-h-[699px] max-w-[1000px] flex justify-between items-start border border-black/15 py-20 px-6 rounded-xl overflow-hidden bg-[#FBFBF8]">
-          {/* Text Section */}
           <div className="flex-1 pl-14 z-10">
             <h2 className="text-2xl md:text-6xl tracking-tighter text-left text-black font-semibold">
               Trust and safety features for your protection
@@ -27,7 +24,7 @@ const Featuress = () => {
                   alt="Circular Background"
                   className="h-full w-full object-cover"
                 />
-                {/* Centered Star inside Featcirc */}
+          
                 <div className="absolute inset-0 flex items-center justify-center">
                   <img
                     src={Star.src}
@@ -37,12 +34,11 @@ const Featuress = () => {
                 </div>
               </div>
 
-              {/* Secure Payments Text */}
+             
               <div className="ml-4 flex flex-col justify-center">
                 <p className="h-[30px] w-[180px] text-lg md:text-xl text-black font-medium">
                   Secure payments
                 </p>
-                {/* Additional Text */}
                 <p className=" h-[25px] w-[430px] text-sm md:text-base text-black mt-1">
                   Only release payment when the task is completed to your
                   satisfaction.
@@ -57,7 +53,7 @@ const Featuress = () => {
                   alt="Circular Background"
                   className="h-full w-full object-cover"
                 />
-                {/* Centered Star inside Featcirc */}
+          
                 <div className="absolute inset-0 flex items-center justify-center">
                   <img
                     src={FeatD.src}
@@ -67,12 +63,12 @@ const Featuress = () => {
                 </div>
               </div>
 
-              {/* Trusted ratings and reviews Text */}
+              
               <div className="ml-4 flex flex-col justify-center">
                 <p className="h-[30px] w-[280px] text-lg md:text-xl text-black font-medium">
                   Trusted ratings and reviews
                 </p>
-                {/* Additional Text */}
+                
                 <p className=" h-[25px] w-[380px] text-sm md:text-base text-black mt-1">
                   Pick the right person for the task based on real ratings and reviews from other users
                 </p>
@@ -86,7 +82,7 @@ const Featuress = () => {
                   alt="Circular Background"
                   className="h-full w-full object-cover"
                 />
-                {/* Centered Star inside Featcirc */}
+               
                 <div className="absolute inset-0 flex items-center justify-center">
                   <img
                     src={FeatD.src}
@@ -96,12 +92,12 @@ const Featuress = () => {
                 </div>
               </div>
 
-              {/* Insurance for peace of mind Text */}
+              
               <div className="ml-4 flex flex-col justify-center">
                 <p className="h-[30px] w-[280px] text-lg md:text-xl text-black font-medium">
                   Insurance for peace of mind
                 </p>
-                {/* Additional Text */}
+               
                 <p className=" h-[25px] w-[430px] text-sm md:text-base text-black mt-1">
                   We provide liability insurance for Taskers performing most task activities
                 </p>
@@ -109,7 +105,6 @@ const Featuress = () => {
             </div>
           </div>
 
-          {/* Image Section */}
           <div className="flex-1 relative z-10 w-[343px] h-full">
             <img
               src={Feat.src}
@@ -119,15 +114,13 @@ const Featuress = () => {
           </div>
         </div>
 
-        {/* Right Side Image with Rounded Corners */}
+       
         <div className="absolute top-0 right-0 h-[699px] w-[400px] rounded-lg overflow-hidden">
           <img
             src={Feat1.src}
             alt="Feat Image"
             className="h-full w-full object-cover"
           />
-
-          {/* Feat2 Image inside Feat1 */}
           <div className="absolute top-20 left-10 h-[76px] w-[180px] z-10 px-5 py-2 rounded-lg">
             <img
               src={Feat2.src}
@@ -144,12 +137,11 @@ const Featuress = () => {
           width: "1300px",
           height: "699px",
           gap: "0px",
-          opacity: "1", // Set opacity to 1 if the component should be visible
+          opacity: "1", 
         }}
       >
-        {/* Content Container */}
+       
         <div className="relative w-full h-full flex items-center justify-center border border-black/15 rounded-xl bg-white">
-          {/* YouTube Video Embed */}
           <iframe
             width="100%"
             height="100%"
